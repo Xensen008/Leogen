ae56db065a32042aa6be1b04839762cdf2b572c1
Arnab jyoti Kakati <106694416+Xensen008@users.noreply.github.com>
2024-08-10 06:45:41 +0000

    port issue fixed

--- Backend/app.js
+++ Backend/app.js
@@ -1,13 +1,12 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = 3000;
+const port = 8080;
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
 app.listen(3000, () => {
-    console.log(`Server is running on port ${port}`);
-    console.log(`http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
+  console.log(`http://localhost:${port}`);
 });
-
