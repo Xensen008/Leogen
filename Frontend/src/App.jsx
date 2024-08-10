7d96cdec8e2889d1907a0d6613924c77de807f4a
Xensen008 <arnabjyotikakati008@gmail.com>
2024-08-10 10:53:20 +0530

    Add dark theme support .create new pages for CreatePost and Home.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,24 @@
+import styled, { ThemeProvider } from "styled-components";
+import { darkTheme } from "./utils/Theme";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  background: ${({ theme }) => theme.bg};
+`;
+
+const Wrapper = styled.div
 function App() {
-  return <>Hello Arnab is cool.. Ai gen</>;
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Container>
+        <Wrapper>
+          hello
+        </Wrapper>
+      </Container>
+    </ThemeProvider>
+  );
 }
 
 export default App;
